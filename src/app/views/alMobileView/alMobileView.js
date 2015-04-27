bbaa994b752ec45f1b7f68796056f843a90c538e
Andy Law <mail@andylaw.info>
2015-04-28 00:17:31 +0100

    Small restructuring of html components and styling fixes

--- src/app/views/alMobileView/alMobileView.js
+++ src/app/views/alMobileView/alMobileView.js
@@ -16,26 +16,73 @@ var mobileView = angular.module('al.angularExample.views.alMobileView', [
 	alMobileInteractive.name
 ]);
 
+/**
+ * @ngdoc directive
+ * @name al.angularExample.views.alMobileView:alMobileView
+ * @restrict E
+ * @scope
+ *
+ * @description
+ * Main view for mobile. Attach HTML template and listen to callbacks from child components
+ *
+ * @example
+ * <doc:example module="al.angularExample.views.alMobileView.alMobileView">
+ 	<doc:source>
+ 		<div></div>
+ 		<style></style>
+ 		<script></script>
+ 	</doc:source>
+ </doc:example>
+ */
 mobileView.directive('alMobileView', function() {
 	return {
 		restrict: 'E',
 		controller: 'alMobileViewCtrl',
 		template: alMobileViewTemplate,
 		replace: true,
-		scope: {
-
-		},
 		link: function(scope, elem, attrs, ctrl) {
 
+			scope.pusherConnected = false;
 
+			/**
+			 * User has input code into alMobileIntro component. Ask controller to send off, and if successful, set to connected to display alMobileInteractive component
+			 * @param data
+			 */
+			scope.onCodeSendRequested = function(connectionCode) {
+				//TODO: Set to loading state to stop client from sending multiple codes
+				ctrl.connectToPusher(connectionCode).then(function() {
+					scope.clientConnected = true;
+				}, function(message) {
+					scope.clientConnected = false;
+					scope.connectionError = message;
+				});
+			};
 
+			/**
+			 * A ball has been 'thrown/flicked' in the alMobileInteractive component. Request controller to send message to pusher with relevant data
+			 * @param data
+			 */
+			scope.onSendBallRequested = function(data) {
+				ctrl.onSendBallMessage(data);
+			};
 		}
 	}
 });
 
+/**
+ * @ngdoc object
+ * @name al.angularExample.views.al:alMobileViewCtrl
+ * @function
+ *
+ * @description
+ * Controller for mobile view.
+ * Connect to pusher service.
+ * Listen to messages sent from other client
+ */
 mobileView.controller('alMobileViewCtrl', function(
 	$scope,
 	$q,
+	$timeout,
 	AlPusherService,
 	PUSHER_CONNECTED,
 	PUSHER_CONNECTION_ERROR,
@@ -49,18 +96,39 @@ mobileView.controller('alMobileViewCtrl', function(
 	pusher.channel.bind(PUSHER_CONNECTED, onPusherConnected);
 	pusher.channel.bind(PUSHER_CONNECTION_ERROR, onPusherConnectError);
 
+	/**
+	 * Connection to pusher service successfully complete
+	 * @param event
+	 */
 	function onPusherConnected(event) {
-		//TODO: new status when loaded
+		$scope.$apply(function() {
+			$scope.pusherConnected = true;
+		});
 	}
 
+	/**
+	 * Error on connecting to pusher service
+	 * @param event
+	 */
 	function onPusherConnectError(event) {
-		//TODO
+		$scope.pusherConnected = false;
+		$scope.pusherErrorMessage = 'Sorry, currently unable to connect to server.';
 	}
 
+	/**
+	 * Send connection code out to other clients connected to pusher, if matched then resolve promise
+	 * @param connectionCode
+	 * @returns {promise|*|promise|promise|Function|promise}
+	 */
 	function connectToPusher(connectionCode) {
 		var deferred = $q.defer();
 
+		var timeout = $timeout(function() {
+			deferred.reject('Unable to find other client with that code, please check and try again');
+		}, 3000);
+
 		pusher.channel.bind(PUSHER_CODE_MATCHED, function(event) {
+			$timeout.cancel(timeout);
 			deferred.resolve();
 			$scope.connectionCode = connectionCode;
 		});
@@ -68,19 +136,17 @@ mobileView.controller('alMobileViewCtrl', function(
 		AlPusherService.sendMessage(PUSHER_SEND_CODE, {code: connectionCode});
 
 		return deferred.promise;
+	}
+
+	this.connectToPusher = function(connectionCode) {
+		return connectToPusher(connectionCode);
 	};
 
-	$scope.onCodeSendRequested = function(data) {
-		//TODO: Set to loading state
-		connectToPusher(data).then(function() {
-			$scope.connected = true;
-		}, function() {
-			$scope.connected = false;
-			$scope.connectionError = 'Unable to connect at the moment. Please try again later.';
-		});
-	};
-
-	$scope.onSendBallRequested = function(data) {
+	/**
+	 * User has flicked a ball at the screen, tell the pusher service to send out message to connected clients
+	 * @param data
+	 */
+	this.onSendBallMessage = function(data) {
 		AlPusherService.sendMessage(PUSHER_REQUEST_BALL_SEND, {params: data, code: $scope.connectionCode});
 	};
 
